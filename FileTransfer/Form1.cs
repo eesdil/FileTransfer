1aa7357dffbf99fd589f80a61fbafe0f51025eeb
Csaba Korosi <kcs@aquantis.net>
2014-01-20 08:41:51 +0100

    Initial

---
+++ FileTransfer/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileTransfer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            MySqlDb db = new MySqlDb(txtMyConnection.Text);
+
+            db.IterationComplete += new EventHandler<MySqlDb.IterationEventArgs>(db_IterationComplete);
+            db.MoveMedia();
+            MessageBox.Show("The transfer is complete", "Media Transfer", MessageBoxButtons.OKCancel, MessageBoxIcon.Asterisk);
+        }
+
+        void db_IterationComplete(object sender, MySqlDb.IterationEventArgs e)
+        {
+            string infoFromWorker = e.iterationNumber;
+            txtMessage.Text += e.iterationNumber + " - " + e.name;
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtMyConnection.Text = "server=192.168.1.179; user id='root'; password='root'; database='test_crm'; pooling=false; default command timeout=120";
+        }
+    }
+}
---
+++ FileTransfer/MySqlDb.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace FileTransfer
+{
+
+
+
+    class MySqlDb
+    {
+
+        MySqlConnection dbConnection;
+
+        public MySqlDb(String myConnection)
+        {
+            dbConnection = new MySqlConnection(myConnection);
+        }
+
+        public class IterationEventArgs : EventArgs
+        {
+            public string iterationNumber { get; set; }
+            public string name { get; set; }
+        }
+
+        public event EventHandler<IterationEventArgs> IterationComplete;
+
+
+        public String MoveMedia()
+        {
+
+            CRMDataContext da = new CRMDataContext();
+            int iteration = 0;
+
+            da.ObjectTrackingEnabled = false;
+            dbConnection.Open();
+
+            da.CommandTimeout = 120;
+
+            try
+            {
+
+                foreach (Media item in da.Medias)
+                {
+                    iteration += 1;
+                    MySqlCommand mySqlCommand = new MySqlCommand("INSERT into media(Id, Item) VALUES (@Id, @Item)", dbConnection);
+
+                    mySqlCommand.Parameters.AddWithValue("@Id", item.Id);
+                    mySqlCommand.Parameters.AddWithValue("@Item", item.Item.ToArray());
+                    mySqlCommand.ExecuteNonQuery();
+
+
+                    IterationEventArgs args = new IterationEventArgs();
+                    args.iterationNumber = iteration.ToString();
+                    args.name = item.Name.ToString();
+                    IterationComplete(this, args);
+                }
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+            finally
+            {
+                dbConnection.Close();
+            }
+
+            return "";
+        }
+    }
+
+}
